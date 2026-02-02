8561c3a22cfd19290bc42a948f6f1d0b3bf2d386
lij <lij@yocal.com>
2026-02-02 15:12:00 +0800

    feat:docker-compose脚本

---
+++ atun-brain-home/src/main/java/com/atun/App.java
@@ -0,0 +1,13 @@
+package com.atun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
